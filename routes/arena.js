b1a9c6d838236973fc3cb8fc988904701bb858a5
Pradeep-React-Node <91178051+Pradeep-React-Node@users.noreply.github.com>
2023-03-28 15:28:22 +0530

    get arena

--- routes/arena.js
+++ routes/arena.js
@@ -2,7 +2,7 @@ const express = require('express');
 const arena = require('../controllers/arena');
 const arenaRouter = new express.Router();
 
-arenaRouter.post('/get_arena', arena.getArenas);
+arenaRouter.get('/get_arena', arena.getArenas);
 arenaRouter.post('/create_arena', arena.createArena);
 arenaRouter.put('/update_arena/:id', arena.updateArena);
 arenaRouter.delete('/delete_arena/:id', arena.deleteArena);
